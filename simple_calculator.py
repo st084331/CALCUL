81d536dd187e84f71d3302e4896853f2dc576173
st084331 <st084331@student.spbu.ru>
2020-09-23 21:40:30 +0600

    Root and power are ready

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,6 @@
+from symbol import power
+import random
+
 class Calculator:
     def __init__(self, init_value=0):
         self.value = init_value
@@ -24,19 +27,18 @@ class Calculator:
         return self
 
     def power(self, *args):
-        ans = 1
-        for x in args:
-            for i in range(x):
-                ans *= self
-        return ans
+        const = self.value
+        for i in args:
+            for x in range(i-1):
+                self.value *= const
+        return self
 
     def root(self, *args):
-        ans = self
-        for x in args:
-            for i in range(x):
-                ans //= self
-        return ans
-
+        const = self.value
+        for i in args:
+            for x in range(i - 1):
+                self.value /= const
+        return self
 
     def __repr__(self):
         return self.value
@@ -44,16 +46,13 @@ class Calculator:
     def __str__(self):
         return str(self.value)
 
-
 def fun(x):
     return x[::-1]
 
 
 if __name__ == '__main__':
-    calculator = Calculator(100)
+    calculator = Calculator(random.randint(0,10))
     print(calculator)
-    print(calculator.add(1, 2, 3, 5.1).multiply(4, 0.123).subtract(4, 1, -100).divide(5, integer_divide=True))
-    print(Calculator(100) + 10)
-    print(10 + Calculator(12))
-    print(Calculator(123) - Calculator(14))
-    print(Calculator(14) / Calculator(2))
+    print(calculator)
+    print(calculator.power(2,3).add(1, 2, 3, 5.1).multiply(4, 0.123).subtract(4, 1, -100).divide(5, integer_divide=True).root(4))
+
