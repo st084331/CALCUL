f66284a99bfa4745587079855e5232f4b4d84ade
st084331 <st084331@student.spbu.ru>
2020-09-23 22:51:40 +0600

    New test

--- test_calculator.py
+++ test_calculator.py
@@ -1,37 +1,31 @@
 import unittest
+import random
+import datetime
 from simple_calculator import Calculator, fun
-
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
-        self.calculator = Calculator(1)
+        self.calculator = Calculator(random.randint(1,100))
 
     def test_add(self):
-        """
-        Test add function with regular data
-        :rtype: object
-        """
-        calc_value = self.calculator.value
-        self.assertEqual(self.calculator.add(1, 2, 3).value, calc_value + 6)
+        calc_value = self.calculator.value + 1 + 2 + 3
+        self.assertEqual(calc_value, self.calculator.add(1, 2, 3).value)
 
     def test_mul(self):
-        calc_value = self.calculator.value
-        self.assertEqual(self.calculator.multiply(5, 2, 90).value, calc_value * 900)
+        calc_value = self.calculator.value * 5 * 2 * 90
+        self.assertEqual(calc_value, self.calculator.multiply(5, 2, 90).value)
 
     def test_divide(self):
-        calc_value = self.calculator.value
-        self.assertEquals(self.calculator.divide(2, 3).value, calc_value / 6)
-
-    def test_file_read(self):
-        with open('file.txt', 'r') as f:
-            text = f.read()
-            self.assertEquals(fun(text), text[::-1])
+        calc_value = self.calculator.value / 2 / 3
+        self.assertEquals(calc_value, self.calculator.divide(2, 3).value)
 
     def kek(self):
         self.assertEqual(1, 2)
 
 
 if __name__ == '__main__':
+    print("Start of the test",datetime.datetime.now())
     unittest.main()
+    print("End of the test", datetime.datetime.now())
 
 
