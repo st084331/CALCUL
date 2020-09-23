7cfc485c0df84a4cdc8f2f53eb1428e46059cc18
st084331 <st084331@student.spbu.ru>
2020-09-23 23:18:25 +0600

    Ready calculator

--- simple_calculator.py
+++ simple_calculator.py
@@ -37,12 +37,14 @@ class Calculator:
                 self.value *= const
         return self
 
-    def root(self, *args):
-        const = self.value
-        for i in args:
-            for x in range(i - 1):
-                self.value /= const
-        return self
+    def root(self):
+        value1 = self.value
+        while True:
+            value2 = (self.value * self.value + value1)//(2 * self.value)
+            if value2 >= self.value:
+                return self
+            self.value = value2
+
 
     def __repr__(self):
         return self.value
@@ -58,5 +60,5 @@ if __name__ == '__main__':
     calculator = Calculator(random.randint(0,100))
     print(calculator)
     print(calculator)
-    print(calculator.power(2,3).add(1, 2, 3, 5.1).multiply(4, 0.123).subtract(4, 1, -100).divide(5, integer_divide=True).root(4))
+    print(calculator.power(2,3).add(1, 2, 3, 5.1).multiply(4, 0.123).subtract(4, 1, -100).divide(5, integer_divide=True).root())
 
